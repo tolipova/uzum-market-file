2bcc7486386f9c52d427c6c33f70a1a7cf8b67fb
Tolipova Shahina <143376267+tolipova@users.noreply.github.com>
2023-10-04 13:41:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def save_items(item_category,item_name,item_seller,item_dastavka,item_price,item
                 'item_name':item_name, 
                 'item_seller':item_seller,
                 'item_dastavka':item_dastavka,
-                'item_price':item_price,
+                'item_price':item_price,    
                 'item_quantity':item_quantity,
                 'item_assessments':item_assessments,
                 'item_order':item_order}
@@ -15,15 +15,15 @@ def save_items(item_category,item_name,item_seller,item_dastavka,item_price,item
 def load_database():
     try:
         with open('item_databas.json','r') as file :
-            lines =  file.readlines()
-            database = [json.loads(line) for line in lines] 
-            return database
+                lines =  file.readlines()
+                database = [json.loads(line) for line in lines] 
+                return database
     except FileNotFoundError :
         return [] 
 def get_user_input ():
     user_input = input("Enter the name of item: ")         
     return user_input
-    
+        
 def ask_item(user_input, database):
     for entry in database:
         if entry['item_name'] == user_input:
@@ -34,13 +34,20 @@ def filter_by_category(category,database)  :
         if entry['item_category'] == category :
             filtred_items.append(entry)  
         return filtred_items   
+def buyer(item_name,item_quantity,item_price,database) :
+    name = input("Enter the name will buy of item: ")   
+    if name == database[item_name]:
+        quantity = int(input(f"enter the quantity of {name} to buy "))
+        if quantity > item_quantity:
+            print("Error! this quantity more a lot")
+        elif quantity <= item_quantity:
+            total_cost = item_price * quantity
+            print(f"You bought {quantity}{item_name}(s)for {total_cost} so'm")
+        else:
+            print("Error!!")    
+            
 
-def total(price,database)  :
-    for entry in database:
-        if entry ['item_price'] == price:
-            return entry    
-
-def main(): 
+def main():
     filter_choice = input("Do you to filter by category? yes/no: ").lower()
     if filter_choice == 'yes':
         category_to_filter = input("Enter the category to filter by: ")   
@@ -50,20 +57,15 @@ def main():
             print(f"filtred news for category {category_to_filter}")
             for entry in filter_items:
                 print(entry)
-                choice = input("if you want to buy some itemsenter the (1)or exit(2): ")
-                if choice == '1':
-                    item_category = input("Enter the will buy category of item: ")
-                    if item_category == database:
-                        item_price = database[item_category]
-                        quantity = int(input(f"Enter the quantity of {item_category} to buy: "))
-                        total_cost = item_price * quantity
-                        print(f"You bought {quantity}{item_category}(s)for {total_cost} so'm")
-                    else:
-                        print("Invalid product number . Please try again.")    
         else:
             print("No item found the spicified category")      
     else:          
         while True:
+            choice = input("Choosing you seller or buyyer: ").lower()
+            if choice == 'seller':
+                print(main)
+            elif choice == 'buyyer' :
+                print(buyer) 
             database = load_database()
             user_input = get_user_input()
             items = ask_item(user_input,database)
